2cc455bf18eb718b61415dc6dd02db1459463f8b
ju-to <088t3748@gmail.com>
2020-10-01 22:35:30 +0900

    Ruby課題（基礎）Q10,Q11を修正

--- task.rb
+++ task.rb
@@ -85,7 +85,7 @@ def q10
 
   # 以下に回答を記載
   foods.each do |food|
-    if food == "うに"
+    if food.include?("うに")
       puts "#{food}は好物です"
     else
       puts "#{food}はまぁまぁ好きです"
@@ -97,9 +97,8 @@ def q11
   sports = ["サッカー", "バスケ", "野球", ["フットサル", "野球"], "水泳", "ハンドボール", ["卓球", "サッカー", "ボルダリング"]]
 
   # 以下に回答を記載
-  new_sports = sports.flatten!.uniq
   puts "ユーザーの趣味一覧"
-  new_sports.each.with_index(1) do |sports_name, i|
+  sports.flatten!.uniq!.each.with_index(1) do |sports_name, i|
     puts "No#{i} #{sports_name}"
   end
 
